ffd79497cb951c8c1284df45348a22e67668bbc1
prehans <prehansgupta@gmail.com>
2025-02-19 16:11:13 +0530

    initial setup

--- SpringProject/src/main/java/com/SpringProject/SpringProjectApplication.java
+++ SpringProject/src/main/java/com/SpringProject/SpringProjectApplication.java
@@ -8,7 +8,7 @@ public class SpringProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProjectApplication.class, args);
-		System.out.println("Hello World !!");
+		System.out.println("Hello World !! for project");
 	}
 
 }
